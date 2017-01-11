03bffb2aff3fcb3c4f9f6506d24519687d9fc910
Jay Raj <jay.r@thinkpalm.com>
2017-01-11 17:30:48 +0530

    updates

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask,render_template
 import pygal
+from pygal.style import Style
 import json
 import time
 app = Flask(__name__)
+
+custom_style = Style(
+        colors=('#991515','#1cbc7c'),
+        background='#d2ddd9'
+        )
 
 # -----------------------------------------
 # Default route for the charting application
@@ -20,9 +26,11 @@ def home():
 def bar():
     with open('bar.json','r') as bar_file:
         data = json.load(bar_file)
+    chart = pygal.Bar(style = custom_style)
     mark_list = [x['mark'] for x in data]
-    chart = pygal.Bar()
+    tourn_list = [x['tournament'] for x in data]
     chart.add('Annual Mark List',mark_list)
+    chart.add('Tournament Score',tourn_list)
     chart.x_labels = [x['year'] for x in data]
     chart.render_to_file('static/images/bar_chart.svg')
     img_url = 'static/images/bar_chart.svg?cache=' + str(time.time())
